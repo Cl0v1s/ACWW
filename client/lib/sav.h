4f3ff68f6e6dfe6dcd19d1937c52dc082be2b038
Clovis <clovis@bdx.town>
2025-04-09 23:36:45 +0200

    feat: checksum ok !

--- client/lib/sav.h
+++ client/lib/sav.h
@@ -36,17 +36,17 @@ bool readSave(const char* filename, char** content) {
 }
 
 void checksum(char *save) {
-    // int checksVar = 0;
-	// for(int index = 0; index < (SAVCOPY_SIZE / 2); index++) {
-	// 	if (index == CHECKSUM_OFFSET / 2) continue;
-    //     int id = 0 + (index * 2); // possible merde
-    //     uint16_t cell = (save[id] << 8) + (save[id + 1] & 0xff); 
-	// 	checksVar = (checksVar + cell) % 0x10000;
-	// }
+    uint16_t checksVar = 0;
+	for(int index = 0; index < SAVCOPY_SIZE; index += 2) {
+		if (index == CHECKSUM_OFFSET) continue;
+        uint16_t cell = (save[index] & 0xFF) |
+        (save[index+1] << 8);
+		checksVar = (checksVar + cell) % 0x10000;
+	}
 
-    // uint16_t checksum = 0x10000 - checksVar; // possible merde
-    // save[CHECKSUM_OFFSET] = checksum >> 8;
-    // save[CHECKSUM_OFFSET + 1] = checksum & 0xFF;
+    uint16_t checksum = 0x10000 - checksVar;
+    save[CHECKSUM_OFFSET+1] = checksum >> 8;
+    save[CHECKSUM_OFFSET] = checksum & 0xFF;
     memmove(save + SAVCOPY_OFFSET, save, SAVCOPY_SIZE);
 }
 
